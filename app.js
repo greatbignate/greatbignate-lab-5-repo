e57b6dba0ed54583cca2fd176219154fb4aa2441
GREATBIGNATE <greatbignate@gmail.com>
2018-08-10 16:26:48 -0700

    question Four complete it was a doozy

--- app.js
+++ app.js
@@ -82,11 +82,30 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+    // Define variables for problem Four -- key outputs for problem four are the input array and sum.
+    var i=0;
+    var sumFour = 0;
+    var arrayFour = [];
+    var stringNum = '0'
+    
+    while (sumArr[i] !== undefined){
+        stringNum = sumArr[i].toString();
+        arrayFour.push(stringNum);
+        if (sumFour === 0) {
+            sumFour = sumArr[i];
+        } else {
+            sumFour = sum(sumFour,sumArr[i])[0];
+        }
+        i++;
+    }
+
+    var messageFour = ''+arrayFour+ ' was passed in as an array of numbers, and ' +sumFour+ ' is their sum.';
+    return [sumFour, messageFour];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
