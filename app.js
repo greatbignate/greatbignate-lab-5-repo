f6bc09ebff749e77b9819db9e3d2ced531bdef7d
GREATBIGNATE <greatbignate@gmail.com>
2018-08-10 15:10:00 -0700

    Problem three complete

--- app.js
+++ app.js
@@ -54,11 +54,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var sumForThree = sum(sum(a,b)[0],c)[0];
+    var prodForThree = multiply(multiply(a,b)[0],c)[0];
+    var thirdElementThree = +a+ ' and ' +b+ ' and ' +c+ ' sum to ' +sumForThree+ '.';
+    var fourthElementThree = 'The product of ' +a+ ' and ' +b+ ' and ' +c+ ' is ' +prodForThree+ '.';
+    return [sumForThree, prodForThree, thirdElementThree, fourthElementThree];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
